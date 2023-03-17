d2139d8f1ecbc9769707bd10567eb57e94909eaa
Muhammad Rifki Widadi <muhammad.rifki@pegipegi.com>
2023-03-17 23:11:06 +0700

    test: add 2 test case for checkbox

--- test/checkbox_test.dart
+++ test/checkbox_test.dart
@@ -4,6 +4,75 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group("Testing Checkbox Widget", () {
+    group(
+      "Testing OnChange capability",
+      () {
+        testWidgets(
+          "Test Checkbox when being tapped, handle onPressed correctly",
+          (WidgetTester tester) async {
+            bool isChecked = true;
+            await tester.pumpWidget(
+              MaterialApp(
+                home: GarethCheckbox(
+                  key: const Key("Checkbox"),
+                  size: const Size(50, 50),
+                  value: isChecked,
+                  borderColor: Colors.grey,
+                  boxColor: Colors.blue,
+                  cornerRadius: 3,
+                  iconSize: 30,
+                  iconColor: Colors.white,
+                  tristate: false,
+                  onChange: (bool? value) {
+                    if (value != null) {
+                      isChecked = value;
+                    }
+                  },
+                ),
+              ),
+            );
+
+            final finder = find.byIcon(Icons.check);
+
+            await tester.tap(finder);
+            await tester.pump();
+
+            expect(isChecked, false);
+          },
+        );
+
+        testWidgets(
+          "Test Checkbox when being disable, the state doest change",
+          (WidgetTester tester) async {
+            bool isChecked = true;
+            await tester.pumpWidget(
+              MaterialApp(
+                home: GarethCheckbox(
+                  key: const Key("Checkbox"),
+                  size: const Size(50, 50),
+                  value: isChecked,
+                  borderColor: Colors.grey,
+                  boxColor: Colors.blue,
+                  cornerRadius: 3,
+                  iconSize: 30,
+                  iconColor: Colors.white,
+                  tristate: false,
+                  onChange: null,
+                ),
+              ),
+            );
+
+            final finder = find.byIcon(Icons.check);
+
+            await tester.tap(finder);
+            await tester.pump();
+
+            expect(isChecked, true);
+          },
+        );
+      },
+    );
+
     group("Testing Checkbox with 2 state", () {
       testWidgets(
         "Testing checkbox when the value is true must show the checked icon",
@@ -139,7 +208,7 @@ void main() {
               home: GarethCheckbox(
                 key: const Key("Checkbox"),
                 size: const Size(50, 50),
-                value: null,
+                value: true,
                 borderColor: Colors.grey,
                 boxColor: Colors.blue,
                 cornerRadius: 3,
@@ -151,9 +220,14 @@ void main() {
             ),
           );
 
-          final finder = find.byIcon(Icons.remove);
+          final finder = find.byIcon(Icons.check);
 
-          expect(finder, findsOneWidget);
+          await tester.tap(finder);
+          await tester.pump();
+
+          final remove = find.byIcon(Icons.remove);
+
+          expect(remove, findsOneWidget);
         },
       );
     });
